b4a15e1b9d889cdead5683ded53d957072e30cd8
¨Mario Castro¨ <mnc634@utexas.edu>
2021-01-07 14:01:10 -0600

    added first table query for departments

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const inquirer = require("inquirer");
 const Database = require("./employees_db");
 const cTable = require("console.table");
+const CFonts = require('cfonts');
+
 
 const database = new Database({
     host: "localhost",
@@ -11,26 +13,68 @@ const database = new Database({
   });
 
 function mainPrompt() {
+    CFonts.say('Employee|Tracker', {
+        font: 'simple',              // define the font face
+        align: 'left',              // define text alignment
+        colors: ['green'],         // define all colors
+        background: 'transparent',  // define the background color, you can also use `backgroundColor` here as key
+        letterSpacing: 1,           // define letter spacing
+        lineHeight: 1,              // define the line height
+        space: true,                // define if the output text should have empty lines on top and on the bottom
+        maxLength: '0',             // define how many character can be on one line
+        gradient: false,            // define your two gradient colors
+        independentGradient: false, // define if you want to recalculate the gradient for each new line
+        transitionGradient: false,  // define if this is a transition between colors directly
+        env: 'node'                 // define the environment CFonts is being executed in
+    });
     return inquirer
         .prompt([
             {
                 type: "list",
-                message: "What would you like to do?",
-                name: "action",
+                message: "Welcome to your Employee Tracker\nWhat would you like to do?",
+                name: "menu",
                 choices: [
-                  "Add department",
-                  "Add employee",
-                  "Add role",
-                  "Remove employee",
-                  "Update employee role",
-                  "View all departments",
-                  "View all employees",
-                  "View all employees by department",
-                  "View all roles",
-                  "Exit"
+                    "View all Company's Departments",
+                    "View all Employees' Roles",
+                    "View all Employees' Information",
+                    "Exit"
                 ]
             }
-        ])
+        ]).then(function(val){
+            switch(val.menu){
+                case "View all Company's Departments":
+                    viewDepartments();
+                break;
+
+                case "View all Employees' Roles":
+                    viewRoles();
+                break;
+
+                case "View all Employees' Information":
+                    viewRoles();
+                break;
+            };
+        });
 };
 
+
+async function viewDepartments() {
+    let query = "SELECT name, id FROM department ORDER BY id";
+    const rows = await database.query(query);
+    
+    console.table(rows);
+
+    return rows;
+};
+
+// function viewDepartments() {
+//     database.query('SELECT department.name, department.id FROM department ORDER BY department.name;',
+//     function(err, res) {
+//         if (err) throw err
+//         console.table(res)
+//         mainPrompt();
+//       });
+// };
+
 mainPrompt();
+
